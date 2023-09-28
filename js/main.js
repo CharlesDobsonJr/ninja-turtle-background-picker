789f45bd899ec02b4d58b8a3c3403c4a68f38f65
Dobson <CHARLES_DOBSON@homedepot.com>
2023-09-28 00:13:46 -0400

    just setting up for Github

---
+++ js/main.js
@@ -0,0 +1,31 @@
+document.querySelector(".raphael").addEventListener("click", picRaphael);
+document.querySelector(".donatello").addEventListener("click", picDonatello);
+document.querySelector(".michelangelo").addEventListener("click", picMichelangelo);
+document.querySelector(".leonardo").addEventListener("click", picLeonardo);
+
+
+
+
+function picRaphael() {
+  document.querySelector(".heroImg").src = "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Raphael.Teenage-Mutant-Ninja-Turtles.webp"
+  document.body.style.backgroundImage = "url('https://e0.pxfuel.com/wallpapers/914/257/desktop-wallpaper-teenage-mutant-ninja-turtles-out-of-the-shadows-pc-movies-ninja-2.jpg')"
+
+}
+function picDonatello() {
+  document.querySelector(".heroImg").src = "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Donatello.Teenage-Mutant-Ninja-Turtles.webp"
+  document.body.style.backgroundImage = "url('https://e0.pxfuel.com/wallpapers/144/849/desktop-wallpaper-donatello-teenage-mutant-ninja-turtles-out-of-the-shadows-space-ninja.jpg')"
+}
+function picMichelangelo() {
+  document.querySelector(".heroImg").src = "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Michaelangelo.Teenage-Mutant-Ninja-Turtles.webp"
+  document.body.style.backgroundImage = "url('https://e0.pxfuel.com/wallpapers/1020/798/desktop-wallpaper-teenage-mutant-ninja-turtles-movie-movies-anime-ninja-turtles.jpg')"
+}
+function picLeonardo() {
+  document.querySelector(".heroImg").src = "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Leonardo.Teenage-Mutant-Ninja-Turtles.webp"
+  document.body.style.backgroundImage = "url('https://e0.pxfuel.com/wallpapers/674/21/desktop-wallpaper-teenage-mutant-ninja-turtles-tmnt-2-2016-out-of-the-shadows.jpg')"
+}
+
+
+
+
+
+
